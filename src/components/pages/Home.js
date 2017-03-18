db36cd18d919228cc0e3474057daeb8c1b25a26a
Alisher778 <internet-docs@mail.ru>
2017-03-17 21:16:34 -0400

    fixed media query issues

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,6 @@ class Home extends Component{
   render() {
     return(
       <div id="home-page">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:900" rel="stylesheet" />
         <div id="intro">Alisher Musurmonov</div>
         <p>Hi, I am a creative Full-stack Web Developer</p>
         <i className="fa fa-angle-double-down animated bounce infinite" aria-hidden="true"></i>
---
+++ src/components/pages/Projects.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Projects extends Component {
+  render() {
+    return(
+        <div className="projects">
+          <ul className="all-projects">
+            <li><a href=""><img src=""/></a></li>
+            <li><a href=""><img src=""/></a></li>
+            <li><a href=""><img src=""/></a></li>
+          </ul>
+        </div>
+      )
+  } 
+}
+
+export default Projects;
